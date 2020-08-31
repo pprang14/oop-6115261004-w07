4a4977665c978c3ab8d0cc15312f633713a3c409
pprang14 <61750419+pprang14@users.noreply.github.com>
2020-08-31 08:22:36 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace oop_6115261004_w07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Nitat", "Ninchawee", 20000, 10, 2, 2);
+            Lecturer l1 = new Lecturer("Nitat", "Ninchawee", 20000, 0, 20, 20, 20, 20, 10);
+            SupportPersonnel S1 = new SupportPersonnel("Nitat", "Ninchawee", 20000, 0, 10, 20, 40);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(l1.ToString());
+            Console.WriteLine(S1.ToString());
+        }
+    }
+}
